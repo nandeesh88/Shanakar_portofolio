41a38789e77b9e87f0d23aaadd96f2e22253d10c
nandeesh88 <nandugowda888@gmail.com>
2025-12-13 23:34:44 +0530

    Force push new folder

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-<<<<<<< HEAD
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/shankar.io/', // 👈 IMPORTANT — use your repo name exactly
-=======
-export default defineConfig({
-  plugins: [react()],
-  base: '/shankar.io/', // 👈 Replace with your actual repo name
->>>>>>> 75a5e3d545aee527a3882635b2d15000fd0b479b
 })
